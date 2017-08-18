4c7450a6d4629cf53fe8b7a1304ee997a9703fbc
RobDana <RobDana@users.noreply.github.com>
2017-08-18 09:12:44 -0400

    Update utils.js
    Refactor for ie11

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,54 +1,78 @@
 // coerce convert som types of data into another type
+
+
+
 export const coerce = {
-  // Convert a string to booleam. Otherwise, return the value without modification, so if is not boolean, Vue throw a warning.
-  boolean: val => (typeof val === 'string' ? val === '' || val === 'true' ? true : (val === 'false' || val === 'null' || val === 'undefined' ? false : val) : val),
+    // Convert a string to booleam. Otherwise, return the value without modification, so if is not boolean, Vue throw a warning.
+  boolean: function boolean(val) {
+    return typeof val === 'string' ? val === '' || val === 'true' ? true : val === 'false' || val === 'null' || val === 'undefined' ? false : val : val;
+  },
   // Attempt to convert a string value to a Number. Otherwise, return 0.
-  number: (val, alt = null) => (typeof val === 'number' ? val : val === undefined || val === null || isNaN(Number(val)) ? alt : Number(val)),
+  number: function number(val) {
+    var alt = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+    return typeof val === 'number' ? val : val === undefined || val === null || isNaN(Number(val)) ? alt : Number(val);
+  },
   // Attempt to convert to string any value, except for null or undefined.
-  string: val => (val === undefined || val === null ? '' : val + ''),
+  string: function string(val) {
+    return val === undefined || val === null ? '' : val + '';
+  },
   // Pattern accept RegExp, function, or string (converted to RegExp). Otherwise return null.
-  pattern: val => (val instanceof Function || val instanceof RegExp ? val : typeof val === 'string' ? new RegExp(val) : null)
+  pattern: function pattern(val) {
+    return val instanceof Function || val instanceof RegExp ? val : typeof val === 'string' ? new RegExp(val) : null;
+  }
 }
 
-export function getJSON (url) {
-  var request = new window.XMLHttpRequest()
-  var data = {}
+export function getJSON(url) {
+  var request = new window.XMLHttpRequest();
+  var data = {};
   // p (-simulated- promise)
   var p = {
-    then (fn1, fn2) { return p.done(fn1).fail(fn2) },
-    catch (fn) { return p.fail(fn) },
-    always (fn) { return p.done(fn).fail(fn) }
-  };
-  ['done', 'fail'].forEach(name => {
-    data[name] = []
-    p[name] = (fn) => {
-      if (fn instanceof Function) data[name].push(fn)
-      return p
+    then: function then(fn1, fn2) {
+      return p.done(fn1).fail(fn2);
+    },
+    catch: function _catch(fn) {
+      return p.fail(fn);
+    },
+    always: function always(fn) {
+      return p.done(fn).fail(fn);
     }
-  })
-  p.done(JSON.parse)
-  request.onreadystatechange = () => {
+  };
+  ['done', 'fail'].forEach(function (name) {
+    data[name] = [];
+    p[name] = function (fn) {
+      if (fn instanceof Function) data[name].push(fn);
+      return p;
+    };
+  });
+  p.done(JSON.parse);
+  request.onreadystatechange = function () {
     if (request.readyState === 4) {
-      let e = {status: request.status}
+      var e = { status: request.status };
       if (request.status === 200) {
         try {
-          var response = request.responseText
+          var response = request.responseText;
           for (var i in data.done) {
-            var value = data.done[i](response)
-            if (value !== undefined) { response = value }
+            var value = data.done[i](response);
+            if (value !== undefined) {
+              response = value;
+            }
           }
         } catch (err) {
-          data.fail.forEach(fail => fail(err))
+          data.fail.forEach(function (fail) {
+            return fail(err);
+          });
         }
       } else {
-        data.fail.forEach(fail => fail(e))
+        data.fail.forEach(function (fail) {
+          return fail(e);
+        });
       }
     }
-  }
-  request.open('GET', url)
-  request.setRequestHeader('Accept', 'application/json')
-  request.send()
-  return p
+  };
+  request.open('GET', url);
+  request.setRequestHeader('Accept', 'application/json');
+  request.send();
+  return p;
 }
 
 export function getScrollBarWidth () {
@@ -81,7 +105,7 @@ export function getScrollBarWidth () {
 }
 
 // return all the translations or the default language (english)
-export function translations (lang = 'en') {
+export function translations () {
   let text = {
     daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
     limit: 'Limit reached ({{limit}} items max).',
@@ -92,58 +116,69 @@ export function translations (lang = 'en') {
     required: 'Required',
     search: 'Search'
   }
-  return window.VueStrapLang ? window.VueStrapLang(lang) : text
+  return window.VueStrapLang ? window.VueStrapLang('en') : text
 }
 
 // delayer: set a function that execute after a delay
 // @params (function, delay_prop or value, default_value)
-export function delayer (fn, varTimer, ifNaN = 100) {
+export function delayer (fn, varTimer, ifNaN) {
+  ifNan = (ifNan) ? ifNan : 100;
   function toInt (el) { return /^[0-9]+$/.test(el) ? Number(el) || 1 : null }
   var timerId
-  return function (...args) {
-    if (timerId) clearTimeout(timerId)
-    timerId = setTimeout(() => {
-      fn.apply(this, args)
-    }, toInt(varTimer) || toInt(this[varTimer]) || ifNaN)
-  }
+  return function () {
+      var _this = this;
+
+      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      if (timerId) clearTimeout(timerId);
+      timerId = setTimeout(function () {
+        fn.apply(_this, args);
+      }, toInt(varTimer) || toInt(this[varTimer]) || ifNaN);
+    };
 }
 
 // Fix a vue instance Lifecycle to vue 1/2 (just the basic elements, is not a real parser, so this work only if your code is compatible with both)
-export function VueFixer (vue) {
-  var vue2 = !window.Vue || !window.Vue.partial
+export function VueFixer(vue) {
+  var vue2 = !window.Vue || !window.Vue.partial;
   var mixin = {
     computed: {
-      vue2 () { return !this.$dispatch }
+      vue2: function vue2() {
+        return !this.$dispatch;
+      }
     }
-  }
+  };
   if (!vue2) {
     if (vue.beforeCreate) {
-      mixin.create = vue.beforeCreate
-      delete vue.beforeCreate
+      mixin.create = vue.beforeCreate;
+      delete vue.beforeCreate;
     }
     if (vue.beforeMount) {
-      vue.beforeCompile = vue.beforeMount
-      delete vue.beforeMount
+      vue.beforeCompile = vue.beforeMount;
+      delete vue.beforeMount;
     }
     if (vue.mounted) {
-      vue.ready = vue.mounted
-      delete vue.mounted
+      vue.ready = vue.mounted;
+      delete vue.mounted;
     }
   } else {
     if (vue.beforeCompile) {
-      vue.beforeMount = vue.beforeCompile
-      delete vue.beforeCompile
+      vue.beforeMount = vue.beforeCompile;
+      delete vue.beforeCompile;
     }
     if (vue.compiled) {
-      mixin.compiled = vue.compiled
-      delete vue.compiled
+      mixin.compiled = vue.compiled;
+      delete vue.compiled;
     }
     if (vue.ready) {
-      vue.mounted = vue.ready
-      delete vue.ready
+      vue.mounted = vue.ready;
+      delete vue.ready;
     }
   }
-  if (!vue.mixins) { vue.mixins = [] }
-  vue.mixins.unshift(mixin)
-  return vue
+  if (!vue.mixins) {
+    vue.mixins = [];
+  }
+  vue.mixins.unshift(mixin);
+  return vue;
 }
